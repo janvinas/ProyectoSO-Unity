55c01d8f8840c4b77cf32dea3f640c9ed32762ec
Carlosm2003 <carlos.munoz.gil@estudiantat.upc.edu>
2024-01-07 11:53:12 +0100

    mute/unmute

--- Assets/Scripts/ControlMusica.cs
+++ Assets/Scripts/ControlMusica.cs
@@ -1,24 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlMusica : MonoBehaviour
 {
     AudioSource audioSource;
 
+    public Sprite iconoSonidoActivado;
+    public Sprite iconoMute;
+
+    private bool estaMute = false;
+    private Image imagen;
+
     void Start()
     {
+        imagen = GetComponent<Image>();
         audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.S)) // Pausar la música al presionar la tecla P
-        {
-            if (audioSource.isPlaying)
-                audioSource.Pause();
-            else
-                audioSource.UnPause();
-        }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (audioSource.isPlaying)
+                    audioSource.Pause();
+                else
+                    audioSource.UnPause();
+                ToggleMute();
+            }
+    }
+    public void OnClick()
+    {
+        ToggleMute();
+    }
+    private void ToggleMute()
+    {
+        estaMute = !estaMute;
+        imagen.sprite = estaMute ? iconoMute : iconoSonidoActivado;
+        if (estaMute)
+            audioSource.Pause();
+
+        else
+            audioSource.UnPause();
     }
 }
